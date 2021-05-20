c13458c2d2167ad825f65bd62b3178b075c852b7
Nikolaev Viktor <maclauddd@gmail.com>
2021-05-20 23:03:47 +0300

    mikro-orm setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello there");
+import { MikroORM } from "@mikro-orm/core";
+import { __PROD__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
+  const post = orm.em.create(Post, { title: "my first post" });
+  await orm.em.persistAndFlush(post);
+
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
+};
+
+main().catch((e) => console.error(e));
---
+++ server/src/mikro-orm.config.ts
@@ -0,0 +1,15 @@
+import { __PROD__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  entities: [Post],
+  dbName: "lireddit",
+  type: "postgresql",
+  debug: !__PROD__,
+} as Parameters<typeof MikroORM.init>[0];
